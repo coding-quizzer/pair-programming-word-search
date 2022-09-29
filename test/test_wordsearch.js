f99836b33e1d1f3bf5200eedd1ac70b22c4dd261
William Gadd <wgadd@sasktel.net>
2022-09-29 01:25:23 +0000

    Made functions to find words in wordsearch with @jeremy.buist

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,7 +3,7 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
+describe("#wordSearch() horizontal", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present (horizontal)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,76 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
-  });
+  })
+
+  it("should return true if the word is present backwards (horizontal)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'A', 'I', 'N', 'D', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  })
 });
+
+  describe ("#wordSearch() vertical", () => {
+
+    it("should return true if the word is present (vertical)", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+        ['S', 'A', 'I', 'N', 'E', 'I', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'I', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'F', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'E', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'L', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'D', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'SEINFELD')
+
+      assert.isTrue(result);
+    });
+    
+    it("should return true if the word is present (vertical up)", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+        ['S', 'A', 'I', 'N', 'D', 'I', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'L', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'F', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'I', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'E', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'S', 'U', 'A', 'L'],
+      ], 'SEINFELD')
+
+      assert.isTrue(result);
+    });
+  });
+
+  /* describe ("#wordSearch() diagonal", () => {
+
+    it("should return true if the word is present (diagonal up right)", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+        ['S', 'V', 'I', 'N', 'E', 'I', 'L', 'D'],
+        ['Y', 'F', 'Q', 'F', 'I', 'D', 'A', 'L'],
+        ['H', 'M', 'J', 'Y', 'I', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'U', 'I', 'E', 'R', 'L'],
+        ['B', 'F', 'Q', 'E', 'E', 'D', 'Y', 'B'],
+        ['U', 'S', 'T', 'W', 'L', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'D', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'SQUID')
+
+      assert.isTrue(result);
+    });
+  }); */
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,77 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const transpose = function(matrix) {
+    const newMatrix = matrix[0].map((L, x) => matrix.map(y => y[x]));
+    return newMatrix;
+  };
+
+const horizontalWordSearch = function (letters, word) {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+    
+  for (l of horizontalJoin) {
+      if (l.search(word) !== -1) return true
+  }
+
+  return false;
 }
 
+const wordSearch = (letters, word) => { 
+    if (horizontalWordSearch(letters, word)) return true;
+
+    const verticalLetters = transpose(letters);
+
+    if (horizontalWordSearch(verticalLetters, word)) return true;
+
+    let reversedLetters = letters;
+    reversedLetters = reversedLetters.map(l => l.reverse());
+
+    if (horizontalWordSearch(reversedLetters, word)) return true;
+
+    let reversedVerticalLetters = verticalLetters;
+    reversedVerticalLetters = reversedVerticalLetters.map(l => l.reverse());
+
+    if (horizontalWordSearch(reversedVerticalLetters, word)) return true;
+
+    // const diagonalUpRightLetters = [];
+
+    // console.log("letters:", letters);
+    
+    /* const maxSum = letters.length + letters[0].length - 1;
+    for (let sum = 0; sum < 16; sum ++) {
+      const innerArray = [];
+      for (let y in letters) {
+        for (let x in letters[y]) {
+          x = parseInt(x);
+          y = parseInt(y);
+
+          if (x + y === sum) {
+            innerArray.push(letters[y][x]);
+          }
+        
+        }
+      }
+
+        console.log(innerArray);
+        diagonalUpRightLetters.push(innerArray)
+    }
+
+    if (horizontalWordSearch(diagonalUpRightLetters, word)) return true;
+     */
+
+    return false;
+}
+
+
+
 module.exports = wordSearch
+
+wordSearch([
+  ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+  ['S', 'V', 'I', 'N', 'E', 'I', 'L', 'D'],
+  ['Y', 'F', 'Q', 'F', 'I', 'D', 'A', 'L'],
+  ['H', 'M', 'J', 'Y', 'I', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'U', 'I', 'E', 'R', 'L'],
+  ['B', 'F', 'Q', 'E', 'E', 'D', 'Y', 'B'],
+  ['U', 'S', 'T', 'W', 'L', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'D', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'SQUID')
+// [[0, 0]], [[0, 1], [1, 0]], [[2, 0], [1, 1], [0, 2]] 
